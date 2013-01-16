5357f890918825045c1b5bb04b06ac9dc7bb4d81
Nigel Horne <njh@bandsman.co.uk>
2013-01-16 02:06:26 +0000

    Import of NHORNE/CGI-Untaint-Facebook-0.08 from CPAN.
    gitpan-cpan-distribution: CGI-Untaint-Facebook
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         NHORNE/CGI-Untaint-Facebook-0.08.tar.gz
    gitpan-cpan-author:       NHORNE
    gitpan-cpan-maturity:     released

--- lib/CGI/Untaint/Facebook.pm
+++ lib/CGI/Untaint/Facebook.pm
@@ -17,11 +17,11 @@ CGI::Untaint::Facebook - Validate a URL is a valid Facebook URL or ID
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
